1cfe9268356ebbecfd1d05ddd005fa247bf95b23
jinilcs <jinilcs@gmail.com>
2018-01-28 16:25:38 -0500

    Initial commit

---
+++ BTree/Btree.py
@@ -0,0 +1,5 @@
+class BTree:
+    def __init__(self, data, left=None, right=None):
+        self._data = data
+        self._left = left
+        self._right = right
---
+++ BTree/symmetry.py
@@ -0,0 +1,21 @@
+def issymmetric(tree):
+    if not tree:
+        return False
+
+    def isamirrorimage(lefttree, righttree):
+        if not lefttree and not righttree:
+            return True
+        elif not lefttree or not righttree:
+            return False
+        elif lefttree._data != righttree._data:
+            return False
+
+        if not isamirrorimage(lefttree._left, righttree._right):
+            return False
+
+        if not isamirrorimage(lefttree._right, righttree._left):
+            return False
+
+        return True
+
+    return isamirrorimage(tree._left, tree._right)
